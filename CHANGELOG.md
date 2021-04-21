b83184e4d450ae7d54020f97a07326cc793d263b
Max M. Awad <maxmax2012@hotmail.com>
2021-04-21 20:58:26 +0800

    CoCreate-Share project

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+## [1.0.2](https://github.com/CoCreate-app/CoCreate-social-share/compare/v1.0.1...v1.0.2) (2021-04-19)
+
+
+### Bug Fixes
+
+* npm publish and cdn deployment ([337ff8c](https://github.com/CoCreate-app/CoCreate-social-share/commit/337ff8c922fc4a4b184278c3277940d87c4c071b))
+
+## [1.0.1](https://github.com/CoCreate-app/CoCreate-social-share/compare/v1.0.0...v1.0.1) (2021-03-29)
+
+
+### Bug Fixes
+
+* Package Paths ([be259d3](https://github.com/CoCreate-app/CoCreate-social-share/commit/be259d3d917d857f1ef231d933ea844bfd244d76))
+
+# 1.0.0 (2021-03-26)
+
+
+### Features
+
+* Initial Release ([0673301](https://github.com/CoCreate-app/CoCreate-social-share/commit/067330144560104f42c8aaaa63598dbcbf66e5d5))
---
+++ src/index.js
@@ -0,0 +1,43 @@
+// import constants
+import { selectorClick, selectorDiv, options } from './config/constants';
+
+// import functions
+import getData from './functions/getData';
+import share from './shares/index';
+
+const CoCreateSocialShare = {
+    
+    //document_URL:'https://server.cocreate.app',
+    
+	init : function(){
+	    let elements = document.querySelectorAll(selectorClick);
+	    elements.forEach(el=>this.initElement(el));
+	},
+	
+	initElement : function (el) {
+	    var that = this;
+	    el.addEventListener("click", function(event){ 
+
+			// Select Div element with dataset to share
+	        event.preventDefault()
+	        let container = el.closest(selectorDiv)
+
+			// Get data to share from attributes
+			const data = getData(el, document, container);
+
+			// Get general url to share from data
+			const network = share(el, data);
+
+			// Popup sub-window to share data
+    	    that.popup(network)
+	    });   
+	},
+	
+	popup : function(network){
+	    window.open(network["url"], '', options+'height='+network["height"]+',width='+network["width"]);
+	}
+
+}
+
+CoCreateSocialShare.init();
+export default CoCreateSocialShare;
